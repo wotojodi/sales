6a06b4bbc641305b59e8ef871c8d435e1658ff7a
wotojodi <100421807+wotojodi@users.noreply.github.com>
2025-05-12 18:07:27 +0200

    Update Generating_data.py

--- Generating_data.py
+++ Generating_data.py
@@ -117,7 +117,7 @@ def create_record():
     }
 
 # Generate dataset
-data = [create_record() for _ in range(50000)]
+data = [create_record() for _ in range(30000)]
 df = pd.DataFrame(data)
 
 # Reorder columns for clarity and decision-making
